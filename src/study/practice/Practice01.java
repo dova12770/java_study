56c57b255f888f64f5f396e7117239be9534bdf6
dova12770 <aaaa2460@naver.com>
2024-10-16 16:58:49 +0900

    자바 시작 변수 개념 gitignore 추가

---
+++ src/study/practice/Practice01.java
@@ -0,0 +1,37 @@
+package study.practice;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int month;
+		
+		int day = 1;
+		
+		month = 10;
+		day = 16;
+		System.out.println("오늘은 "+ month + "월 " + day + "일 입니다.");
+		
+		String name = "김도현";
+		System.out.println(name);
+		
+		String number ="010-123-5678";
+		System.out.println("전화번호는 " + number + " 입니다.");
+		
+		/*6:
+		 *  b. 특수문자는 "$", "_"를 사용할 수 있음
+		 * c. 숫자로 시작하였음
+		 * f. b와 동일
+		 * g. int는 변수명이라 사용불가
+		 */
+		/*
+		 * 7. c 
+		 */
+		/*
+		 * 8. b
+		 */
+		 
+	}
+
+}
