6bc2a13c36a4a0b56ad18ea4c84bc9f316da3331
dova12770 <aaaa2460@naver.com>
2024-12-31 11:06:09 +0900

    데이터 로직 퀴즈

---
+++ src/study/quiz/practice1231.java
@@ -0,0 +1,56 @@
+package study.quiz;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class practice1231 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] paricipant = {"leo","kiki","eden"};
+		String [] paricipant1 = {"marina","josipa","nikola","vinko","filipa"};
+		String [] paricipant2 = {"mislav","stanko","misiav","ana"};
+		
+		String [] completion = {"eden","kiki"};
+		String [] completion1 = {"josipa","filipa","marina","nikola"};
+		String [] completion2 = {"stanko","ana","misiav"};
+
+//		HashMap<String,Integer> maraton = new HashMap<>();
+//		for(String runner : paricipant) maraton.put(runner, maraton.getOrDefault(runner, 0)+1);
+//		for(String runner : paricipant1) maraton.put(runner, maraton.getOrDefault(runner, 0)+1);
+//		for(String runner : paricipant2) maraton.put(runner, maraton.getOrDefault(runner, 0)+1);
+//		
+//		for(String runner : completion) maraton.put(runner, maraton.get(runner)-1);
+//		for(String runner : completion1) maraton.put(runner, maraton.get(runner)-1);
+//		for(String runner : completion2) maraton.put(runner, maraton.get(runner)-1);
+//		
+//		for(String key : maraton.keySet()) {
+//			if(maraton.get(key)!=0) {
+//				System.out.println(key);
+//			}
+//		}
+//		
+
+//		Set<String>compSet = new HashSet<String>();
+//		for(String s : completion) {
+//			compSet.add(s);
+//		}
+//		
+//		for(String s : paricipant) {
+//			if(compSet.contains(s)) {
+//				compSet.remove(s);
+//			} else {
+//				return s;
+//			}
+//		}
+//		return null;
+	}
+
+
+
+}
+	
