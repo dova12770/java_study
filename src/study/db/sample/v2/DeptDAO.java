ddbe59f206bc1ef1e960501d9a937ca31bb9bcc4
dova12770 <aaaa2460@naver.com>
2025-01-06 15:26:03 +0900

    db update 처리

--- src/study/db/sample/v2/DeptDAO.java
+++ src/study/db/sample/v2/DeptDAO.java
@@ -14,6 +14,42 @@ public class DeptDAO {
 	PreparedStatement psmt;
 	ResultSet rs;
 	
+	
+	//public int modifyDept(int deptno,String dname, String loc) {
+	public int modifyDept(Dept dept) {
+		//			modifyDept("재무팀","부산")
+		conn = DBConnectionManager.connectDB();
+
+		int result =0;
+		// 쿼리 준비
+		String sqlQuery = " update dept "
+						+ "set dname =?,loc=? "
+						+ "where deptno =?";
+		try {
+			// 쿼리 실행후 데이터 후속 처리
+
+			psmt = conn.prepareCall(sqlQuery);
+
+			// 파라미터 셋팅
+			psmt.setString(1, dept.getDname());
+			psmt.setString(2, dept.getLoc());
+			psmt.setInt(3, dept.getDeptno());
+
+			//select -> executeQuery
+			//insert, update, delete -> executeUpdate
+			
+			result = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBConnectionManager.disconnectDB(conn, psmt, rs);
+		}
+
+		return result;
+
+	}
+	
 	//insert update delete	-> 실행수 적용된 행의 갯수
 //	public int saveDept(int deptno, String dname, String loc) {
 	public int saveDept(Dept dept) {
